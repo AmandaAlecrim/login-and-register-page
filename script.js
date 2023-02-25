a875538d059525959cf7444aa81332671f780fc0
Amanda Demétrio Alecrim <amanda2668@hotmail.com>
2023-02-24 21:48:42 -0300

    Adicionando ações de abrir e fechar os formulários

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector('.wrapper');
 const linkLogin = document.querySelector('.link-login');
 const linkRegistro = document.querySelector('.link-registro');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
 linkRegistro.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -8,4 +10,12 @@ linkRegistro.addEventListener('click', () => {
 
 linkLogin.addEventListener('click', () => {
     wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
 });
